80c1241bc7a6efb83910b9da94bc72258535f32a
andreasblomqvist <adde-16@hotmail.com>
2020-06-02 13:21:34 +0200

    Send email

--- lib/screens/book_information.dart
+++ lib/screens/book_information.dart
@@ -1,15 +1,20 @@
 import 'package:bookit_app/functions/displayDeliveryText.dart';
 import 'package:bookit_app/models/new_book.dart';
-import 'package:bookit_app/utils/snackbar.dart';
+import 'package:bookit_app/utils/formattedDate.dart';
 import 'package:bookit_app/widgets/Defaults/DefaultButton.dart';
 import 'package:bookit_app/widgets/defaults/DefaultHeader.dart';
 import 'package:bookit_app/widgets/defaults/DefaultSubHeader.dart';
 import 'package:flutter/material.dart';
 
+import 'package:bookit_app/utils/snackbar.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
+
 class BookInformation extends StatelessWidget {
   final NewBook data;
 
-  const BookInformation({Key key, this.data}) : super(key: key);
+  BookInformation({Key key, this.data}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -43,11 +48,35 @@ class BookInformation extends StatelessWidget {
           child: DefaultButton(
             title: "Skicka bytesförfrågan",
             onPressed: () {
-              createSnackBar(context, "Förfrågan skickad");
+              sendMail();
+              Navigator.of(context).pop();
             },
           ),
         ),
       ],
     );
+  }
+
+  sendMail() async {
+    String u = "bookit.message@gmail.com";
+    String p = "Sommar2020";
+    final smtpServer = gmail(u, p);
+
+    //TODO: Alert, asking if you agree on sending your email address to the recipent.
+    //TODO: Uppdatera email för användaren.
+    //TODO: Current user.
+    final message = Message()
+      ..from = Address(u)
+      ..recipients.add('adde-16@hotmail.com')
+      ..subject =
+          'BookIt App - En ny bytesförfrågan på ${data.title}! - ${formattedDate()}'
+      ..text =
+          'Det har kommit in ett nytt bytesförslag på ${data.title} av ${data.author}.\nAnvändaren har email adressen: ..., skicka ett email och diskutera vidare.';
+    try {
+      final sendReport = await send(message, smtpServer);
+      print('Message sent: ' + sendReport.toString());
+    } on MailerException catch (e) {
+      print('Message not sent. \n' + e.toString());
+    }
   }
 }
